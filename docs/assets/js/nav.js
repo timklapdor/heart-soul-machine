d2cab56448ede5e3b4617658abe35ee5c0790047
Tim Klapdor <tim.klapdor@gmail.com>
2026-04-10 00:53:45 +0930

    adding in darkmode and fixing nav

--- docs/assets/js/nav.js
+++ docs/assets/js/nav.js
@@ -1,14 +1,16 @@
-var toggle = document.querySelector('#toggle');
-var menu = document.querySelector('#menu');
-var menuItems = document.querySelectorAll('#menu li a');
+document.addEventListener('DOMContentLoaded', function () {
+  var toggle = document.querySelector('#toggle');
+  var menu = document.querySelector('#menu');
 
-toggle.addEventListener('click', function(){
-  if (menu.classList.contains('is-active')) {
-    this.setAttribute('aria-expanded', 'false');
-    menu.classList.remove('is-active');
-  } else {
-    menu.classList.add('is-active'); 
-    this.setAttribute('aria-expanded', 'true');
-    //menuItems[0].focus();
-  }
+  if (!toggle || !menu) return;
+
+  toggle.addEventListener('click', function(){
+    if (menu.classList.contains('is-active')) {
+      this.setAttribute('aria-expanded', 'false');
+      menu.classList.remove('is-active');
+    } else {
+      menu.classList.add('is-active'); 
+      this.setAttribute('aria-expanded', 'true');
+    }
+  });
 });
